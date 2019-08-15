a6d6708d6ac77b8c5beb0bb78307b8f3bae3a538
Ruby King <ruby.sz.king@gmail.com>
2019-08-15 16:20:50 -0400

    mongoose running

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+mongoose.connection.on('error', err => console.log(err.message + 'is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+mongoose.connect('mongodb://localhost:27017/holidays', {useNewUrlParser: true})
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoose..')
+})
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
