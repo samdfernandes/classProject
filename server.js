1de19564e502e5c696b8e80b589bee01485331f8
Ruby King <ruby.sz.king@gmail.com>
2019-08-15 16:19:52 -0400

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3003;
+const mongoose = require('mongoose')
+const cors = require('cors')
+const whitelist = ['http://localhost:3000']
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1) {
+//         callback(null,true)
+//     } else {
+//         callback(new Error('Not allowed by CORS'))
+//     }
+//     }
+// }
+
+// app.use(cors(corsOptions))
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World')
